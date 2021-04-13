ed95348498c754988b7e84ccf16b348eaf22477f
Borja Candela Salinas <65466303+BorjaCandela@users.noreply.github.com>
2021-04-13 15:38:28 +0200

    Update AAFK.ps1
    Missed assembly 🥇

--- AAFK.ps1
+++ AAFK.ps1
@@ -1,6 +1,6 @@
 Write-Host "Moving!"
 $minutes = 300
-
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 for ($i = 0; $i -lt $minutes; $i++) {
     Start-Sleep -Seconds 60
     $Pos = [System.Windows.Forms.Cursor]::Position
