2867b316ca65925e8e55f2c6ef8d2014c05ef852
Borja Candela Salinas <65466303+BorjaCandela@users.noreply.github.com>
2021-04-13 15:32:59 +0200

    Update AAFK.ps1
    Switched the while-true for a long for

--- AAFK.ps1
+++ AAFK.ps1
@@ -1,6 +1,7 @@
-﻿Write-Host "Moviendo!"
+Write-Host "Moving!"
+$minutes = 300
 
-while ($true){
+for ($i = 0; $i -lt $minutes; $i++) {
     Start-Sleep -Seconds 60
     $Pos = [System.Windows.Forms.Cursor]::Position
     [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X) + 1) , $Pos.Y)
