755e0ded066b49b6d571e0feb121eaa466fc5ae4
Borja Candela Salinas <65466303+BorjaCandela@users.noreply.github.com>
2021-04-13 15:15:07 +0200

    This script will move the pixel once each minute to prevent the screensaver :)
    Keep in mind that you company configured the screensaver timeout for security reasons.
    
    All credits to:
    https://dmitrysotnikov.wordpress.com/2009/06/29/prevent-desktop-lock-or-screensaver-with-powershell/

---
+++ AAFK.ps1
@@ -0,0 +1,7 @@
+﻿Write-Host "Moviendo!"
+
+while ($true){
+    Start-Sleep -Seconds 60
+    $Pos = [System.Windows.Forms.Cursor]::Position
+    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point((($Pos.X) + 1) , $Pos.Y)
+}
