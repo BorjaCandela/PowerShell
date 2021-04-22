61c846a1c306bd6bafd447c0c477f26350dc8f4d
Borja Candela Salinas <65466303+BorjaCandela@users.noreply.github.com>
2021-04-22 11:39:36 +0200

    To export as OVA/OVF a single VM from ESXi
    Unavailable with Free License

---
+++ GetMachine.ps1
@@ -0,0 +1,22 @@
+﻿if (Get-Module -ListAvailable -Name Posh-SSH) {
+# To change the certification policy
+# Set-PowerCLIConfiguration -InvalidCertificateAction Prompt
+# Get-PowerCLIConfiguration
+
+# Read hostname/ip address and VM name
+    $esxiHostIP = Read-Host ("ESXi IP Address")
+    $vmName = Read-Host ("Virtual Machine name")
+    Connect-VIServer $esxiHostIP
+
+# It does not work with free license
+    Get-Snapshot $vmName | Remove-Snapshot -confirm:$false # Delete snapshots
+    Get-VM -Name $vmName | Shutdown-VMGuest -confirm:$false # Shutdown machine
+    Get-VM -Name $vmName | Get-CDDrive | Set-CDDrive -NoMedia -confirm:$false # Remove CD
+    Get-VM -Name $vmName | Export-VApp -Destination C:\Users\$env:UserName\Desktop -Format OVA|OVF # Export as OVA/OVF
+} 
+else {
+    Write-Host "Please install PowerCli: 
+        Install-Module -Name Posh-SSH
+    And retry again."
+}
+
