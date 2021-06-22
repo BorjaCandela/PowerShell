f0314e15e7dbeab6733ee74fc63b6b4b7fe341eb
BorjaCandela <b.candela.salinas@gmail.com>
2021-06-22 17:00:18 +0200

    Updated the param names of RESTCall

--- RESTCall.ps1
+++ RESTCall.ps1
@@ -1,7 +1,7 @@
     $url = "https://remotesite.dom/restapp?param1="    # REST Method url
     $param1 = Read-Host "Insert the value of the param1"    # The script ask for the value of the first parameter
     $param2 = "&param2=value2"    # Second Parameter could be hardcoded....
-    $call = "$url$text$voice"
+    $call = "$url$param1$param2"
 
     # Write-Host  "$call"    # To test the entire URI string
     # $result = Invoke-RestMethod -Uri "$call"-Method Get   # Dropping the response into a variable if you need it
