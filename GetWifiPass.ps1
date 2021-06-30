4892f5084ba3d226b816c4cc8de2eb3538ee3eaf
BorjaCandela <b.candela.salinas@gmail.com>
2021-06-30 11:07:09 +0200

    Actualizado GetWifiPass

--- GetWifiPass.ps1
+++ GetWifiPass.ps1
@@ -1,4 +1,13 @@
-﻿clear
-netsh wlan show profiles
-$net = Read-Host "Insert the network name to get the key"
-netsh wlan show profile name=$net key=clear
+﻿while($true)
+{
+    clear
+    netsh wlan show profiles
+    $net = Read-Host "Inserta el nombre de la red de la que quieras su contraseña o pulsa Q para salir"
+    if($net -eq "q" -or $net -eq "Q"){
+        exit        
+    } else {
+        netsh wlan show profile name=$net key=clear | Select-String -Pattern 'Contenido de la clave'
+        Write-Host ""
+        Read-Host -Prompt "Pulsa intro para comprobar otra clave"
+    }
+}
